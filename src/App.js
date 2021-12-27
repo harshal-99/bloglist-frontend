24591546853f3c1d31b0ccd846049ab363330c42
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-12-27 18:37:06 +0530

    Exercise 5.1 done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Blog        from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+	const [ blogs, setBlogs ] = useState([])
+	const [ username, setUsername ] = useState('')
+	const [ password, setPassword ] = useState('')
+	const [ user, setUser ] = useState(null)
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs(blogs)
+		)
+	}, [])
+
+	const loginForm = () => (
+		<form onSubmit={null}>
+			<div>username
+				<input
+					type="text"
+					value={username}
+					onChange={({target}) => setUsername(target.value)}
+				/>
+			</div>
+			<div>password
+				<input
+					type="password"
+					value={password}
+					onChange={({target}) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">login</button>
+		</form>
+	)
+
+	return (
+		<div>
+			<h2>blogs</h2>
+			{user === null ? loginForm() : <div>
+				<p>{user.name} logged-in</p>
+			</div>}
+			{blogs.map(blog =>
+				<Blog key={blog.id} blog={blog}/>
+			)}
+		</div>
+	)
 }
 
-export default App;
+export default App
