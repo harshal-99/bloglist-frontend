f7d98d3febec41407a5bae4784c4ed267d873ff5
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-12-30 19:31:15 +0530

    Exercise 5.4 done.
    Implement notification to inform success and unsuccessful operations.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import React, { useState, useEffect } from 'react'
 import Blog         from './components/Blog'
 import blogService  from './services/blogs'
 import loginService from './services/login'
+import Notification from "./components/Notification";
 
 const App = () => {
 	const [ blogs, setBlogs ] = useState([])
+	const [ user, setUser ] = useState(null)
+
 	const [ username, setUsername ] = useState('')
 	const [ password, setPassword ] = useState('')
-	const [ user, setUser ] = useState(null)
 
 	const [ title, setTitle ] = useState('')
 	const [ author, setAuthor ] = useState('')
 	const [ url, setUrl ] = useState('')
 
+	const [ message, setMessage ] = useState(null)
+	const [ className, setClassName ] = useState('')
 
 	useEffect(() => {
 		blogService.getAll().then(blogs =>
@@ -46,8 +50,18 @@ const App = () => {
 			setUser(user)
 			setUsername('')
 			setPassword('')
-		} catch (e) {
 
+			setMessage('login success')
+			setClassName('success')
+			setTimeout(() => {
+				setMessage(null)
+			}, 5000)
+		} catch (e) {
+			setMessage(e.response.data.error)
+			setClassName('error')
+			setTimeout(() => {
+				setMessage(null)
+			}, 5000)
 		}
 	}
 
@@ -88,6 +102,12 @@ const App = () => {
 				setUrl('')
 			})
 			.catch(error => console.log(error))
+
+		setMessage(`a new blog ${title} by ${author} added`)
+		setClassName('success')
+		setTimeout(() => {
+			setMessage(null)
+		}, 5000)
 	}
 
 	const createNewBlog = () => {
@@ -113,6 +133,7 @@ const App = () => {
 	return (
 		<div>
 			<h2>blogs</h2>
+			<Notification message={message} className={className}/>
 			{user === null ? loginForm() : <div>
 				<p>{user.name} logged in <button
 					onClick={() => {
