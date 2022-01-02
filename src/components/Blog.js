1a30d1a74de86001d3f23249b95f1a11a6799569
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-01-02 13:05:02 +0530

    Exercise 5.7 done.
    Add a button to each blog to toggle details.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({blog}) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5
+	}
+
+	const [ view, setView ] = useState(true)
+
+	const viewLabel = view ? 'view' : 'hide'
+
+	const toggleView = () => {
+		setView(!view)
+	}
+
+	const compactView = () => (
+		<div>
+			{blog.title}
+			<button onClick={toggleView}>{viewLabel}</button>
+		</div>
+	)
+	const fullView = () => (
+		<div>
+			<div>{blog.title}
+				<button onClick={toggleView}>{viewLabel}</button>
+			</div>
+			<div>{blog.url}</div>
+			<div>
+				likes {blog.likes}
+				<button>like</button>
+			</div>
+			<div>{blog.author}</div>
+		</div>
+	)
+
+	return (
+		<div style={blogStyle}>
+			{view ? compactView() : fullView()}
+		</div>
+	)
+}
 
 export default Blog
